f62c6119eebbadbfef8021e73257b30a877c1326
RaSan <34002411+RaSan147@users.noreply.github.com>
2022-09-17 04:00:46 +0600

    #31
    need more improvement

--- src/local_server (py~3.4).py
+++ src/local_server (py~3.4).py
@@ -108,7 +108,7 @@ class Tools:
 		tt = ""
 		for i in text.split("\n"):
 			tt += i.center(term_col) + "\n"
-		return (f"\n\n{s*term_col}\n{tt}{s*term_col}\n\n")
+		return ("\n\n%s\n%s%s\n\n"%(s*term_col, tt, s*term_col))
 
 tools = Tools()
 config = Config()
@@ -142,12 +142,22 @@ import tempfile, random, string, json
 
 import traceback
 
-import pkg_resources as pkg_r, importlib
+import importlib
 
 
 def get_installed():
-	importlib.reload(pkg_r)
-	return [pkg.key for pkg in pkg_r.working_set]
+	use_pkg = 0 # py 3.4 compat
+	try:
+		import pkg_resources as pkg_r
+		importlib.reload(pkg_r)
+		use_pkg = 1
+	except ImportError:
+		pass
+
+	if use_pkg:
+		importlib.reload(pkg_r)
+		return [pkg.key for pkg in pkg_r.working_set]
+	return list(sys.modules.keys())
 
 
 
@@ -190,6 +200,12 @@ def init_requirements():
 			if config.get_os()=='Windows':
 				if 'pip' in missing:
 					subprocess.call([sys.executable, '-m', 'ensurepip'])
+					if sys.version_info<(3,5,0):
+						subprocess.call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip==19.1.1'])
+					else:
+						subprocess.call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+					# WILL NOT WORK ON PY 3.4 SINCE PIP USE F-STRINGS
+
 					if "pip" in get_installed():
 						missing.remove("pip")
 
@@ -471,9 +487,9 @@ word-wrap: break-word;
 
 
 .pagination {
-  cursor: pointer;
-  width: 150px;
-  max-width: 800px
+	cursor: pointer;
+	width: 150px;
+	max-width: 800px
 }
 
 .pagination {
@@ -498,9 +514,9 @@ word-wrap: break-word;
 
 .pagination:active {
 	position: relative;
-  top: 4px;
-  left: 4px;
-  box-shadow: none
+	top: 4px;
+	left: 4px;
+	box-shadow: none
 }
 
 
@@ -568,10 +584,10 @@ _js_script = """
 		<input type="hidden" name="post-type" value="upload">
 		<input type="hidden" name="post-uid" value="12345">
 
-  <p>PassWord:&nbsp;&nbsp;</p><input name="password" type="text" label="Password"><br>
-  <p>Load File:&nbsp;&nbsp;</p><input name="file" type="file" multiple/><br><br>
+	<p>PassWord:&nbsp;&nbsp;</p><input name="password" type="text" label="Password"><br>
+	<p>Load File:&nbsp;&nbsp;</p><input name="file" type="file" multiple/><br><br>
 
-  <input type="submit" value="&#10174; upload" style="background-color: #555; height: 30px; width: 100px"></form>
+	<input type="submit" value="&#10174; upload" style="background-color: #555; height: 30px; width: 100px"></form>
 
 <br>
 <p id="task"></p>
@@ -1207,9 +1223,141 @@ import time
 import urllib.parse
 import contextlib
 from functools import partial
-from http import HTTPStatus
+# from http import HTTPStatus
 
 import re
+
+from enum import IntEnum
+
+class HTTPStatus(IntEnum):
+    """3.4 comp"""
+    def __new__(cls, value, phrase, description=''):
+        obj = int.__new__(cls, value)
+        obj._value_ = value
+
+        obj.phrase = phrase
+        obj.description = description
+        return obj
+
+    # informational
+    CONTINUE = 100, 'Continue', 'Request received, please continue'
+    SWITCHING_PROTOCOLS = (101, 'Switching Protocols',
+            'Switching to new protocol; obey Upgrade header')
+    PROCESSING = 102, 'Processing'
+    EARLY_HINTS = 103, 'Early Hints'
+
+    # success
+    OK = 200, 'OK', 'Request fulfilled, document follows'
+    CREATED = 201, 'Created', 'Document created, URL follows'
+    ACCEPTED = (202, 'Accepted',
+        'Request accepted, processing continues off-line')
+    NON_AUTHORITATIVE_INFORMATION = (203,
+        'Non-Authoritative Information', 'Request fulfilled from cache')
+    NO_CONTENT = 204, 'No Content', 'Request fulfilled, nothing follows'
+    RESET_CONTENT = 205, 'Reset Content', 'Clear input form for further input'
+    PARTIAL_CONTENT = 206, 'Partial Content', 'Partial content follows'
+    MULTI_STATUS = 207, 'Multi-Status'
+    ALREADY_REPORTED = 208, 'Already Reported'
+    IM_USED = 226, 'IM Used'
+
+    # redirection
+    MULTIPLE_CHOICES = (300, 'Multiple Choices',
+        'Object has several resources -- see URI list')
+    MOVED_PERMANENTLY = (301, 'Moved Permanently',
+        'Object moved permanently -- see URI list')
+    FOUND = 302, 'Found', 'Object moved temporarily -- see URI list'
+    SEE_OTHER = 303, 'See Other', 'Object moved -- see Method and URL list'
+    NOT_MODIFIED = (304, 'Not Modified',
+        'Document has not changed since given time')
+    USE_PROXY = (305, 'Use Proxy',
+        'You must use proxy specified in Location to access this resource')
+    TEMPORARY_REDIRECT = (307, 'Temporary Redirect',
+        'Object moved temporarily -- see URI list')
+    PERMANENT_REDIRECT = (308, 'Permanent Redirect',
+        'Object moved permanently -- see URI list')
+
+    # client error
+    BAD_REQUEST = (400, 'Bad Request',
+        'Bad request syntax or unsupported method')
+    UNAUTHORIZED = (401, 'Unauthorized',
+        'No permission -- see authorization schemes')
+    PAYMENT_REQUIRED = (402, 'Payment Required',
+        'No payment -- see charging schemes')
+    FORBIDDEN = (403, 'Forbidden',
+        'Request forbidden -- authorization will not help')
+    NOT_FOUND = (404, 'Not Found',
+        'Nothing matches the given URI')
+    METHOD_NOT_ALLOWED = (405, 'Method Not Allowed',
+        'Specified method is invalid for this resource')
+    NOT_ACCEPTABLE = (406, 'Not Acceptable',
+        'URI not available in preferred format')
+    PROXY_AUTHENTICATION_REQUIRED = (407,
+        'Proxy Authentication Required',
+        'You must authenticate with this proxy before proceeding')
+    REQUEST_TIMEOUT = (408, 'Request Timeout',
+        'Request timed out; try again later')
+    CONFLICT = 409, 'Conflict', 'Request conflict'
+    GONE = (410, 'Gone',
+        'URI no longer exists and has been permanently removed')
+    LENGTH_REQUIRED = (411, 'Length Required',
+        'Client must specify Content-Length')
+    PRECONDITION_FAILED = (412, 'Precondition Failed',
+        'Precondition in headers is false')
+    REQUEST_ENTITY_TOO_LARGE = (413, 'Request Entity Too Large',
+        'Entity is too large')
+    REQUEST_URI_TOO_LONG = (414, 'Request-URI Too Long',
+        'URI is too long')
+    UNSUPPORTED_MEDIA_TYPE = (415, 'Unsupported Media Type',
+        'Entity body in unsupported format')
+    REQUESTED_RANGE_NOT_SATISFIABLE = (416,
+        'Requested Range Not Satisfiable',
+        'Cannot satisfy request range')
+    EXPECTATION_FAILED = (417, 'Expectation Failed',
+        'Expect condition could not be satisfied')
+    IM_A_TEAPOT = (418, 'I\'m a Teapot',
+        'Server refuses to brew coffee because it is a teapot.')
+    MISDIRECTED_REQUEST = (421, 'Misdirected Request',
+        'Server is not able to produce a response')
+    UNPROCESSABLE_ENTITY = 422, 'Unprocessable Entity'
+    LOCKED = 423, 'Locked'
+    FAILED_DEPENDENCY = 424, 'Failed Dependency'
+    TOO_EARLY = 425, 'Too Early'
+    UPGRADE_REQUIRED = 426, 'Upgrade Required'
+    PRECONDITION_REQUIRED = (428, 'Precondition Required',
+        'The origin server requires the request to be conditional')
+    TOO_MANY_REQUESTS = (429, 'Too Many Requests',
+        'The user has sent too many requests in '
+        'a given amount of time ("rate limiting")')
+    REQUEST_HEADER_FIELDS_TOO_LARGE = (431,
+        'Request Header Fields Too Large',
+        'The server is unwilling to process the request because its header '
+        'fields are too large')
+    UNAVAILABLE_FOR_LEGAL_REASONS = (451,
+        'Unavailable For Legal Reasons',
+        'The server is denying access to the '
+        'resource as a consequence of a legal demand')
+
+    # server errors
+    INTERNAL_SERVER_ERROR = (500, 'Internal Server Error',
+        'Server got itself in trouble')
+    NOT_IMPLEMENTED = (501, 'Not Implemented',
+        'Server does not support this operation')
+    BAD_GATEWAY = (502, 'Bad Gateway',
+        'Invalid responses from another server/proxy')
+    SERVICE_UNAVAILABLE = (503, 'Service Unavailable',
+        'The server cannot process the request due to a high load')
+    GATEWAY_TIMEOUT = (504, 'Gateway Timeout',
+        'The gateway server did not receive a timely response')
+    HTTP_VERSION_NOT_SUPPORTED = (505, 'HTTP Version Not Supported',
+        'Cannot fulfill request')
+    VARIANT_ALSO_NEGOTIATES = 506, 'Variant Also Negotiates'
+    INSUFFICIENT_STORAGE = 507, 'Insufficient Storage'
+    LOOP_DETECTED = 508, 'Loop Detected'
+    NOT_EXTENDED = 510, 'Not Extended'
+    NETWORK_AUTHENTICATION_REQUIRED = (511,
+        'Network Authentication Required',
+        'The client needs to authenticate to gain network access')
+
 
 
 def get_dir_size(start_path = '.', limit=None, return_list= False, full_dir=True):
@@ -1795,7 +1943,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 	def __init__(self, *args, directory=None, **kwargs):
 		if directory is None:
 			directory = os.getcwd()
-		self.directory = directory #os.fspath(directory)
+		self.directory = directory #os.fspath(directory) is same
 		super().__init__(*args, **kwargs)
 
 	def do_GET(self):
@@ -1984,6 +2132,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 								preline = line
 								
 				except IOError:
+					traceback.print_exc()
 					return (False, "Can't create file to write, do you have permission to write?")
 				
 
@@ -2340,8 +2489,9 @@ tr:nth-child(even) {
 			# RELOADS THE SERVER BY RE-READING THE FILE, BEST FOR TESTING REMOTELY. VULNERABLE
 			reload = True
 
-			httpd.server_close()
-			httpd.shutdown()
+			if sys.version_info>=(3,5,0):
+				httpd.server_close()
+				httpd.shutdown()
 
 		elif spathsplit[-1].startswith('dlY%3F'):
 			msg = False
@@ -2491,7 +2641,7 @@ tr:nth-child(even) {
 				except UnicodeDecodeError:
 					displaypath = urllib.parse.unquote(path)
 				displaypath = html.escape(displaypath, quote=False)
-				
+
 
 
 				title = self.get_titles(displaypath)
@@ -2953,6 +3103,7 @@ var player = new Plyr('#player', { controls });
 
 
 
+import socket
 
 def _get_best_family(*address):
 	infos = socket.getaddrinfo(
@@ -3001,15 +3152,15 @@ def test(HandlerClass=BaseHTTPRequestHandler,
 	HandlerClass.protocol_version = protocol
 	httpd = ServerClass(addr, HandlerClass)
 	host, port = httpd.socket.getsockname()[:2]
-	url_host = f'[{host}]' if ':' in host else host
+	url_host = '[%s]'%host if ':' in host else host
 	hostname = socket.gethostname()
 	local_ip = config.IP if config.IP else get_ip()
 	config.IP= local_ip
 
 	print(
-		f"Serving HTTP on {host} port {port} \n" #TODO: need to check since the output is "Serving HTTP on :: port 6969"
-		f"(http://{url_host}:{port}/) ...\n" #TODO: need to check since the output is "(http://[::]:6969/) ..."
-		f"Server is probably running on {config.address()}"
+		"Serving HTTP on %s port %s \n"%(host, port), #TODO: need to check since the output is "Serving HTTP on :: port 6969"
+		"(http://%s:%s/) ...\n"%(url_host, port), #TODO: need to check since the output is "(http://[::]:6969/) ..."
+		"Server is probably running on " + config.address()
 
 	)
 	try:
@@ -3089,4 +3240,5 @@ if __name__ == '__main__':
 
 if reload == True:
 	subprocess.call([sys.executable, config.MAIN_FILE] + sys.argv[1:])
+	#							3.4 compatibility ~~~~~^^^^^^^^^^^^^^~~
 	sys.exit(0)
