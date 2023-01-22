c1765e578d0ada6374d723ad0f163d0bc162b960
RaSan <34002411+RaSan147@users.noreply.github.com>
2023-01-23 01:04:14 +0600

    ugh forgot to copy paste from dev_src. V0.6.3 coming up

--- dev_src/local_server.py
+++ dev_src/local_server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = "0.6.1"
+__version__ = "0.6.3"
 enc = "utf-8"
 __all__ = [
 	"HTTPServer", "ThreadingHTTPServer", "BaseHTTPRequestHandler",
