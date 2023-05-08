253df0040b76b2bdbc73600bedfa32bb02cc79d0
transiscodev MacBook Air <github@yallinthehall.33mail.com>
2023-05-07 17:51:55 -0700

    Rewrite for checkbox permissions

--- dev_src/test_user_mgmt.py
+++ dev_src/test_user_mgmt.py
@@ -1,6 +1,7 @@
 import pytest
 import pickledb
 import os, sys
+from typing import Tuple
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from dev_src.pyroboxCore import config
@@ -34,13 +35,15 @@ def test_class_User_init(monkeypatch):
     demo_user_pw = "mydoghasfleas"
     demo_user_dn = "AliceTheAdmin"
     demo_user = User(
-        username=demo_user_dn, permission=UserPermission(7), password=demo_user_pw
+        username=demo_user_dn, permission=UserPermission(1), password=demo_user_pw
     )
 
     assert type(demo_user) == User
-    assert type(demo_user.permission) == UserPermission
+    assert type(demo_user.permission) == int
+    assert demo_user.permission == 2
+    assert type(demo_user.get_permissions()) == tuple
+    assert type(demo_user.get_permissions()[0]) == UserPermission
     assert demo_user.username == demo_user_dn
-    assert demo_user.permission == UserPermission(7)
 
     with pytest.raises(ValueError):
         User(username="non_exsistant", permission=UserPermission(1))
@@ -54,9 +57,10 @@ def test_class_User_lookup(monkeypatch):
     demo_user_dn = "AliceTheAdmin"
     demo_user = User.get_user(demo_user_dn)
     assert type(demo_user) == User
-    assert type(demo_user.permission) == UserPermission
+    assert type(demo_user.permission) == int
+    assert type(demo_user.get_permissions()) == tuple
     assert demo_user.username == demo_user_dn
-    assert demo_user.permission == UserPermission(7)
+    assert UserPermission(1) in demo_user.get_permissions()
 
 
 def test_class_User_password(monkeypatch):
@@ -98,22 +102,48 @@ def test_class_User_permissions(monkeypatch):
     monkeypatch.setattr("dev_src.user_mgmt.user_db", test_user_db)
     demo_user_dn = "AliceTheAdmin"
     demo_user = User.get_user(demo_user_dn)
-    demo_user.set_permissions(permission=40)  # invalid permission
-    assert demo_user.permission == UserPermission(7)
+    demo_user.revoke(UserPermission(3))
+    assert demo_user.permission == 2
+    assert type(demo_user.get_permissions()) == tuple
+    assert type(demo_user.get_permissions()[0]) == UserPermission
+    assert UserPermission(1) in demo_user.get_permissions()
 
-    with pytest.raises(ValueError):
-        demo_user.set_permissions(permission=UserPermission(40))  # invalid permission
-    assert demo_user.permission == UserPermission(7)
+    demo_user.revoke(UserPermission(1))
+    assert demo_user.permission == 0
+    assert type(demo_user.get_permissions()) == tuple
+    assert type(demo_user.get_permissions()[0]) == UserPermission
+    assert UserPermission(0) in demo_user.get_permissions()
 
-    demo_user.set_permissions(permission=4)  # invalid permission
-    demo_user.set_permissions(permission=UserPermission(4))  # valid permission
-    assert demo_user.permission == UserPermission(4)
+    demo_user.permit(UserPermission(1))
+    demo_user.permit(UserPermission(3))
+    assert demo_user.permission == 10
+    assert type(demo_user.get_permissions()) == tuple
+    assert type(demo_user.get_permissions()[0]) == UserPermission
+    assert UserPermission(1) in demo_user.get_permissions()
+    assert UserPermission(3) in demo_user.get_permissions()
 
+    another_user = User(
+        username="Charlie",
+        permission=(UserPermission(3), UserPermission(5)),
+        password="somep@ssword",
+    )
+    assert another_user.permission == 40
+    assert type(another_user.get_permissions()) == tuple
+    assert type(another_user.get_permissions()[0]) == UserPermission
+    assert UserPermission(3) in another_user.get_permissions()
+    assert UserPermission(5) in another_user.get_permissions()
+
+
+def test_class_User_creds(monkeypatch):
+    test_user_db: pickledb.PickleDB = pickledb.load(
+        os.path.join(config.MAIN_FILE_dir, "test_users.db"), True
+    )
+    monkeypatch.setattr("dev_src.user_mgmt.user_db", test_user_db)
     user_bob = User("bob", UserPermission(0), "theP@ssword")
     assert user_bob.check_creds("theP@ssword")
     assert not user_bob.check_creds("NOTtheP@ssword")
 
 
 def test_enum_UserPermission():
-    for each in range(0, 7):
+    for each in range(0, 6):
         assert UserPermission(each)
--- dev_src/user_mgmt.py
+++ dev_src/user_mgmt.py
@@ -5,6 +5,7 @@ import os
 from secrets import compare_digest
 from pyroboxCore import config, logger
 from enum import Enum
+from typing import Tuple, List
 
 # Loads user database. Database is plaintext but stores passwords as a hash salted by config.PASSWORD
 user_db: pickledb.PickleDB = pickledb.load(
@@ -13,26 +14,32 @@ user_db: pickledb.PickleDB = pickledb.load(
 
 
 class UserPermission(Enum):
-    """Enum for WebUI user permissions, in Unix permission style
+    """Enum for WebUI user permissions, inspired by Unix permission style
 
     Args:
-        Enum (int): POSIX Octal Permission for User
+        Enum (int): Permission code for user
     """
 
     NOPERMISSION = 0
-    EXECUTE = 1
-    WRITE = 2
-    WRITE_EXECUTE = 3
-    READ = 4
-    READ_EXECUTE = 5
-    READ_WRITE = 6
-    READ_WRITE_EXECUTE = 7
+    READ = 1
+    DOWNLOAD = 2
+    MODIFY = 3
+    DELETE = 4
+    UPLOAD = 5
+    ZIP = 6
+
+    # [ ] view [ ] download [ ] zip download (since it uses great resources) [ ] upload [ ] delete [ ] modify [ ] banned (maybe?)
 
 
 class User:
     """Object for WebUI users"""
 
-    def __init__(self, username: str, permission: UserPermission, password: str = None):
+    def __init__(
+        self,
+        username: str,
+        permission: UserPermission | Tuple[UserPermission],
+        password: str = None,
+    ):
         """Generate Object for WebUI users
 
         Args:
@@ -72,34 +79,117 @@ class User:
         else:
             if username and permission and password:
                 update_pw(self, password)
-                self.set_permissions(permission)
+                self.permission = 0
+                self.permit(permission)
             else:
                 raise ValueError("No such username")
 
-    common_salt = hashlib.md5(
-        config.PASSWORD.encode()
-    ).hexdigest()  # get the MD5 has of the CLI password to use as a salt
+    common_salt = hashlib.md5(config.PASSWORD.encode()).hexdigest()
+    # get the MD5 has of the CLI password to use as a salt, makes a longer string and avoids holding secrets in memory
 
     def get_salt_pw(self, password: str) -> str:
+        """Method to generate salted and hashes password
+
+        Args:
+            password (str): plaintext password to be salted
+
+        Returns:
+            str: Checksum of password concat'ed to the MD5 hash of config.PASSWORD
+        """
         return hashlib.sha256(password.encode() + self.common_salt.encode()).hexdigest()
+
+    def unpack_permission(self, packed: int) -> List[int]:
+        """Unpacks permission as int -> list of binary/bool switches like [0,1,0,0,1,0]
+
+        Args:
+            packed (int): permission stored as an integer in the object and db
+
+        Returns:
+            List[int]: list of binary switches to be changed
+        """
+        return [packed >> index & 1 for index in range(0, 6)]
+
+    def pack_permission(self, unpacked: List[int]) -> int:
+        """Packs permissions from an ordered list of binary switches to an integer for storage in memory/object
+
+        Args:
+            unpacked (List[int]): list of binary switched that were modified
+
+        Returns:
+            int: permission stored as an integer in the object and db
+        """
+        packed = 0
+        for index, each in enumerate(unpacked):
+            packed |= each << index
+        return packed
 
     @classmethod
     def get_user(cls, username: str):
         if user_db.exists(username) and user_db.exists(username + "__permissions"):
-            permission = UserPermission(int(user_db.get(username + "__permissions")))
+            permission = user_db.get(username + "__permissions")
             return cls(username=username, permission=permission)
         else:
             logger.error(f"User {username} does not exist")
             return False
 
-    def set_permissions(self, permission: UserPermission):
-        if type(permission) != UserPermission:
-            return 2
-        self.permission = permission
-        user_db.set(self.username + "__permissions", permission.value)
-        return 0
+    def get_permissions(self) -> Tuple[UserPermission]:
+        """Get searchable permissions tuple
+
+        Returns:
+            Tuple[UserPermission]: Tuple that can be checked with `UserPermission(5) in user.get_permission()` to discern if user has given permission
+        """
+        output = []
+        for index, each in enumerate(self.unpack_permission(self.permission)):
+            if each:
+                output.append(UserPermission(index))
+        if output.__len__() == 0:
+            output.append(UserPermission(0))
+        return tuple(output)
+
+    def permit(self, permission: UserPermission | Tuple[UserPermission]):
+        """Turn on permissions
+
+        Args:
+            permission (UserPermission | Tuple[UserPermission]): Single UserPermission to enable, or tuple of several
+        """
+        standing_permission = self.unpack_permission(self.permission)
+        if type(permission) == UserPermission:
+            permission = (permission,)
+        for each in permission:
+            standing_permission[each.value] = 1
+        self.permission = self.pack_permission(standing_permission)
+        user_db.set(
+            self.username + "__permissions", self.pack_permission(standing_permission)
+        )
+
+    def revoke(self, permission: UserPermission | Tuple[UserPermission]):
+        """Turn off permissions
+
+        Args:
+            permission (UserPermission | Tuple[UserPermission]): Single UserPermission to disable, or tuple of several
+        """
+        standing_permission = self.unpack_permission(
+            user_db.get(self.username + "__permissions")
+        )
+        if type(permission) == UserPermission:
+            permission = (permission,)
+        for each in permission:
+            standing_permission[each.value] = 0
+        self.permission = self.pack_permission(standing_permission)
+        user_db.set(
+            self.username + "__permissions", self.pack_permission(standing_permission)
+        )
 
     def reset_pw(self, old_password: str, new_password: str) -> int:
+        """Reset password
+
+        Args:
+            old_password (str): Old plaintext password for confirmation before change
+            new_password (str): New plaintext password to be saved
+
+        Returns:
+            int: 0 if old_password accepted, else 1
+        """
         salted_old_password = self.get_salt_pw(old_password)
         salted_new_password = self.get_salt_pw(new_password)
         if compare_digest(user_db.get(self.username), salted_old_password):
@@ -111,5 +201,13 @@ class User:
             return 1
 
     def check_creds(self, password: str) -> bool:
+        """Check credentials
+
+        Args:
+            password (str): Password as plaintext
+
+        Returns:
+            bool: Was password valid?
+        """
         salted_new_password = self.get_salt_pw(password)
         return compare_digest(user_db.get(self.username), salted_new_password)
