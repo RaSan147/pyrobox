c44abb8794c5bf5ad91db7ba53167dedf61d255f
RaSan147 <34002411+RaSan147@users.noreply.github.com>
2023-06-08 19:40:03 +0600

    Update pyroDB.py

--- dev_src/pyroDB.py
+++ dev_src/pyroDB.py
@@ -38,11 +38,13 @@ import sys
 import os
 import signal
 import shutil
-# import json
+from collections.abc import Iterable
 import time
-from random import random
+import random
 from tempfile import NamedTemporaryFile
 from threading import Thread
+
+from typing import Union
 
 from tabulate import tabulate
 import msgpack
@@ -55,14 +57,22 @@ class PickleDB(object):
 
 	key_string_error = TypeError('Key/name must be a string!')
 
-	def __init__(self, location, auto_dump=True, sig=True):
+	def __init__(self, location="", auto_dump=True, sig=True):
 		'''Creates a database object and loads the data from the location path.
 		If the file does not exist it will be created on the first update.
 		'''
 		self.db = {}
-		self.load(location, auto_dump)
+		
+		self.in_memory = False
+		self.location = ""
+		if location:
+			self.load(location, auto_dump)
+		else:
+			self.in_memory = True
+
 		self.dthread = None
 
+		self.auto_dump = auto_dump
 		if sig:
 			self.set_sigterm_handler()
 
@@ -94,27 +104,41 @@ class PickleDB(object):
 		
 	def new(self):
 		self.db = {}
+		
+	def _fix_location(self, location):
+		location = os.path.expanduser(location)
+		return location
 
 	def load(self, location, auto_dump):
 		'''Loads, reloads or Changes the path to the db file'''
-		location = os.path.expanduser(location)
-		self.loco = location
+		self.in_memory = False
+		
+		self.location = self._fix_location(location)
+
 		self.auto_dump = auto_dump
 		if os.path.exists(location):
 			self._loaddb()
 		else:
 			self.new()
 		return True
+		
+	def set_location(self, location):
+		self.location = location
+		self.in_memory = False
 
 	def _dump(self):
 		'''Dump to a temporary file, and then move to the actual location'''
 		with NamedTemporaryFile(mode='wb', delete=False) as f:
 			msgpack.dump(self.db, f)
 		if os.stat(f.name).st_size != 0:
-			shutil.move(f.name, self.loco)
+			shutil.move(f.name, self.location)
 
 	def dump(self):
 		'''Force dump memory db to file'''
+		
+		if self.in_memory:
+			return  
+			
 		self.dthread = Thread(target=self._dump)
 		self.dthread.start()
 		self.dthread.join()
@@ -126,11 +150,11 @@ class PickleDB(object):
 	def _loaddb(self):
 		'''Load or reload the json info from the file'''
 		try:
-			with open(self.loco, 'rb') as f:
+			with open(self.location, 'rb') as f:
 				db = msgpack.load(f)
 				self.db = db
 		except ValueError:
-			if os.stat(self.loco).st_size == 0:  # Error raised because file is empty
+			if os.stat(self.location).st_size == 0:  # Error raised because file is empty
 				self.new()
 			else:
 				raise  # File is not empty, avoid overwriting it
@@ -142,7 +166,8 @@ class PickleDB(object):
 
 
 	def validate_key(self, key):
-		"""Make sure key is a string"""
+		"""Make sure key is a string
+		msgpack is optimized for string keys, so we need to make sure"""
 
 		if not isinstance(key, (str, bytes)):
 			raise self.key_string_error
@@ -348,12 +373,14 @@ class PickleDB(object):
 		
 
 class PickleTable(object):
-	def __init__(self, filename, *args, **kwargs):
+	def __init__(self, filename="", *args, **kwargs):
 		self.CC = 0 # consider it as country code and every items are its people. they gets NID
-		self.CC = hash(time.time() + random() + time.thread_time())
+		
+		self.gen_CC()
+		
 		
 		self.busy = False
-		self.pk = PickleDB(filename, *args, **kwargs)
+		self._pk = PickleDB(filename, *args, **kwargs)
 		
 		
 		
@@ -361,19 +388,26 @@ class PickleTable(object):
 		
 		self.ids = [h for h in range(self.height)]
 		
+	def gen_CC(self):
+		self.CC = hash(time.time() + random.random() + time.thread_time())
+		
+		return self.CC
 	
 	def get_height(self):
-		h = len(self.pk[self.column_names[0]]) if self.column_names else 0
+		h = len(self._pk[self.column_names[0]]) if self.column_names else 0
 
 		return h
 		
 	def __str__(self):
 		# header = self.column_names
-		x = tabulate([self.row(i) for i in range(min(self.height, 500))], headers="keys", tablefmt="orgtbl")
+		x = tabulate([self.row(i) for i in range(min(self.height, 50))], headers="keys", tablefmt="orgtbl")
 		if self.height > 50:
 			x += "\n..."
 			
 		return x
+		
+	def set_location(self, location):
+		self._pk.set_location(location)
 		
 	
 	def lock(self, func):
@@ -391,15 +425,18 @@ class PickleTable(object):
 		return inner
 		
 	def column(self, name):
-		return self.pk.db[name]
+		return self._pk.db[name]
 	
 	def columns(self):
 		'''Return a list of all columns in db'''
-		return self.pk.db
+		return self._pk.db
 	
 	@property
 	def column_names(self):
-		return list(self.pk.db.keys())
+		"""
+		return a tuple (unmodifiable) of column names
+		"""
+		return tuple(self._pk.db.keys())
 		
 	def add_column(self, name, exist_ok=False, AD=True):
 		"""
@@ -407,50 +444,123 @@ class PickleTable(object):
 		exist_ok: ignore if column already exists. Else raise KeyError
 		AD: auto-dump
 		"""
+		self._pk.validate_key(key=name)
+
 		tsize = self.height
 		if name in self.column_names:
 			if exist_ok :
-				tsize = self.height- len(self.pk.db[name])
+				tsize = self.height - len(self._pk.db[name])
 				if not tsize: # 0 cells to add
 					return
 			else:
 				raise KeyError("Column Name already exists")
 		else:
-			self.pk.db[name] = []
+			self._pk.db[name] = []
+			self.gen_CC() # major change
 		
-		self.pk.db[name].extend([None] * tsize)
+		
+		self._pk.db[name].extend([None] * tsize)
+		
 		
 		if AD:
 			self.auto_dump()
 		
 	
-	def del_colum(self, name, AD=True):
+	def del_column(self, name, AD=True):
 		"""
-		 @ locked
+		@ locked
 		# name: colum to delete
 		# AD: auto dump
 		"""
-		self.lock(self.pk.db.pop)(name)
+		self.lock(self._pk.db.pop)(name)
+		if not self._pk.db: # has no keys
+			self.height = 0
+			
+		# self.gen_CC()
 		
 		if AD:
 			self.auto_dump()
 		
 	
-	def rows(self):
-		'''Return a list of all rows in db'''
-		headers = self.column_names
-		for i in range(self.height):
-			yield [self.pk.db[j][i] for j in headers]
+	
+	def row(self, row, _columns=()):
+		columns = _columns or self.column_names
+		return {j: self._pk.db[j][row] for j in columns}
 
-	def row(self, row):
+	def row_obj(self, row, _columns=()):
 		'''Return a row object `_PickleTRow` in db
 		# row: row index
 		'''
-		headers = self.column_names
-		return _PickleTRow(self,
-			self.ids[row], 
-			self.CC,
-			{j: self.pk.db[j][row] for j in headers})
+		return _PickleTRow(source=self,
+			uid=self.ids[row],
+			CC=self.CC)
+			
+	def row_obj_by_id(self, row_id):
+		'''Return a row object `_PickleTRow` in db
+		# row: row index
+		'''
+		return _PickleTRow(source=self,
+			uid=row_id,
+			CC=self.CC)
+			
+	def rows(self):
+		'''Return a list of all rows in db'''
+		columns = self.column_names
+		for r in range(self.height):
+			yield self.row(r, _columns=columns)
+			
+	def rows_obj(self):
+		'''Return a list of all rows in db'''
+		columns = self.column_names
+		for r in range(self.height):
+			yield self.row_obj(r, _columns=columns)
+			
+			
+	def search_iter(self, kw, column=None , row=None, full_match=False, return_obj=True):
+		"""
+		search a keyword in a cell/row/column/entire sheet and return the row object 
+		"""
+		if return_obj:
+			ret = self.get_cell_obj
+		else:
+			ret = self.get_cell
+			
+		def check(item, is_in):
+			if full_match or not isinstance(is_in, Iterable):
+				return is_in == item
+				
+			return item in is_in
+			
+
+		if column and row:
+			cell = self.get_cell(column, row)
+			if check(kw, cell):
+				yield ret(col=column, row=row)
+			return None
+			
+		elif column:
+			for r, i in enumerate(self.column(column)):
+				if check(kw, i):
+					yield ret(col=column, row=r)
+				
+			return None
+					
+		elif row:
+			_row = self.row(row)
+			for c, i in _row.items():
+				if check(kw, i):
+					yield ret(col=c, row=row)
+					
+			return None 
+			
+		else:
+			for col in self.column_names:
+				for r, i in enumerate(self.column(col)):
+					if check(kw, i):
+						yield ret(col=col, row=r)
+				
+				
+				
 		
 
 		
@@ -461,16 +571,50 @@ class PickleTable(object):
 		# val: value of cell
 		# AD: auto dump
 		"""
-		self.pk.db[col][row] = val
+		self._pk.db[col][row] = val
 		
 		if AD:
 			self.auto_dump()
+			
+	def set_cell_by_id(self, col, row_id, val, AD=True):
+		"""
+		# col: column name
+		# row_id: unique row id
+		# val: value of cell
+		# AD: auto dump
+		"""
+		return self.set_cell(col=col, row=self.ids.index(row_id), val=val, AD=AD) 
+			
+	def get_cell(self, col, row):
+		"""
+		get cell value
+		"""
+		return self._pk.db[col][row]
 		
+	def get_cell_by_id(self, col, row_id):
+		return self.get_cell(col, self.ids.index(row_id))
+		
+	def get_cell_obj(self, col, row=-1, row_id=-1):
+		"""
+		return cell object 
+		# col: column name
+		# row: row index (use euther row or row_id)
+		# row_id: unique id of the row
+		"""
+		
+		if row>-1:
+			return _PickleTCell(self, column=col, uid=self.ids[row], CC=self.CC)
+		elif row_id>-1:
+			return _PickleTCell(self, column=col, uid=self.ids[row], CC=self.CC)
+		else:
+			raise IndexError("Invalid row")
+
+
 	def pop_row(self, index=-1, AD=True):
 		self.ids.pop(index)
 		
 		for c in self.column_names:
-			self.pk.db[c].pop(index)
+			self._pk.db[c].pop(index)
 			
 		self.height -=1
 		
@@ -492,10 +636,16 @@ class PickleTable(object):
 
 	
 	def _add_row(self, row:dict):
-		self.ids.append(self.ids[-1] + 1)
+		"""
+		# row: row must be a dict containing column names and values
+		"""
+		if not self.ids:
+			self.ids.append(0)
+		else:
+			self.ids.append(self.ids[-1] + 1)
 			
 		for k in self.column_names:
-			self.pk.db[k].append(None)
+			self._pk.db[k].append(None)
 			
 		
 			
@@ -520,51 +670,136 @@ class PickleTable(object):
 			self.auto_dump()
 			
 		
-		
-			
+	def verify_source(self, CC):
+		return CC == self.CC
+	 
+	def raise_source(self, CC):
+		if not self.verify_source(CC):
+			raise KeyError("Database has been updated drastically. Row index will mismatch!") 
 		
 		
 						
 	def dump(self):
-		self.pk.dump()
+		self._pk.dump()
 		
 	def auto_dump(self):
-		self.pk._autodumpdb()
+		self._pk._autodumpdb()
+		
+class _PickleTCell(object):
+	def __init__(self, source:PickleTable, column, uid, CC):
+		self.source = source
+		self.id = uid
+		self.column = column
+		self.CC = CC
+		
+	@property
+	def value(self):
+		self.source.raise_source(self.CC)
+
+		return self.source.get_cell_by_id(self.column, self.id)
+
+	def __str__(self):
+		return str({
+			"value": self.value,
+			"column": self.column,
+			"row": self.row
+		})
+		
+	def __eq__(self, other):
+		if isinstance(other, self.__class__):
+			return self.value == other.value
+		
+		return self.value==other
+		
+	def set(self, value, AD=True):
+		self.source.raise_source(self.CC)
+		
+		self.source.set_cell_by_id(self.column, self.id, val=value, AD=AD)
+		
+	@property
+	def row(self):
+		self.source.raise_source(self.CC)
+		
+		return self.source.ids.index(self.id)
+	
+	def row_obj(self):
+		self.source.raise_source(self.CC)
+		
+		return self.source.row_obj_by_id(row_id=self.id)
 		
 		
-class _PickleTRow(dict):
-	def __init__(self, source:PickleTable, uid, CC, items):
+class _PickleTRow:
+	def __init__(self, source:PickleTable, uid, CC):
 		self.source = source
 		self.id = uid
 		self.CC = CC
-		super().__init__(items)
 		
-	def verify_source(self):
-		return self.source.CC == self.CC
-	
-	def raise_source(self):
-		if not self.verify_source():
-			raise KeyError("Database has been updated drastically. Row index will mismatch!")
+	def __getitem__(self, name):
+		# Auto dumps
+		self.source.raise_source(self.CC)
+		
+		self.source.get_cell(name, self.source.ids.index(self.id))
+
+	def __setitem__(self, name, value):
+		# Auto dumps
+		self.source.raise_source(self.CC)
+		
+		self.source.set_cell(name, self.source.ids.index(self.id), value)
 
 	def __delitem__(self, name):
-		# No auto dump
-		self.raise_source()
+		# Auto dump
+		self.source.raise_source(self.CC)
 		
-		self.source._set_cell(name, self.source.ids.index(self.id), None)
+		self.source.set_cell(name, self.source.ids.index(self.id), None)
 		
+	def update(self, new:Union[dict, "_PickleTRow"], ignore_extra=False):
+		"""
+		Auto dumps 
+		"""
+		
+		for k, v in new.items():
+			try:
+				self.source._set_cell(k, self.source.ids.index(self.id), v)
+			except KeyError:
+				if not ignore_extra:
+					raise
+					
+		self.source.auto_dump()
+		
+	def __str__(self):
+		return str({k:v for k, v in self.items()})
+		
+	def keys(self):
+		return self.source.column_names
+		
+	def items(self):
+		for k in self.keys():
+			yield (k, self[k])
 		
 	def del_row(self):
 		# Auto dumps
+		self.source.raise_source(self.CC)
+
 		self.source.del_row_id(self.id)
+
+	
+		
+		
 		
 		
 
-if __name__ != "__main__": 
-	import msgpack, time, os
+import string
+def Lower_string(length): # define the function and pass the length as argument  
+    # Print the string in Lowercase  
+    result = ''.join((random.choice(string.ascii_lowercase) for x in range(length))) # run loop until the define length 
+    return result
+
+
+if __name__ != "__main__":
 	
 	st = time.time()
 	for i in range(10):
-		p = PickleDB("__test.mdb")
+		p = PickleDB("__test.pdb")
 		p.set("nn", ['spam', 'eggs']*1000)
 		
 		p = PickleDB("__test.pdb")
@@ -578,7 +813,7 @@ if __name__ == "__main__":
 
 	
 	st = time.time()
-	tb = PickleTable("test.mdb")
+	tb = PickleTable()
 	print(f"load time: {time.time()-st}s")
 	
 	print(tb.height)
@@ -588,8 +823,8 @@ if __name__ == "__main__":
 	tb.add_column("Y", 1)
 	
 	print("adding")
-	#for n in range(555555):
-		#tb._add_row({"x":n, "Y":00})
+	for n in range(int(5555)):
+		tb._add_row({"x":n, "Y":00})
 		
 		#print(n)
 	
@@ -600,9 +835,37 @@ if __name__ == "__main__":
 	tb.dump()
 	print(f"dump time: {time.time()-dt}s") 
 	
-	#print(tb)
+	print(tb)
 	print("Total cells", tb.height * len(tb.column_names))
 	
 	et = time.time()
-	print(et - st)
+	print(f"Load and dump test in {et - st}s")
+	
+
+	
+	print("\n Assign test")
+	st = time.time()
+	
+	for row in tb.rows_obj():
+		row["m"] = Lower_string(10)
+		
+	et = time.time()
+		
+	print(f"Assigned test in {et - st}s")
+	print(tb)
+	
+	
+	
+	print("\n\n Search test")
+	st = time.time()
+	
+	for cell in tb.search_iter(kw="abc", column="m"):
+		print(cell)
+		
+	et = time.time()
+		
+	print(f"Search test in {et - st}s")
+	
+	
+	
 	
