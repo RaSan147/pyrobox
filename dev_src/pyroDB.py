9559dab6c922415984e6f92d1fcdd32ee5d77ff3
RaSan147 <34002411+RaSan147@users.noreply.github.com>
2023-06-10 17:04:07 +0600

    Update pyroDB.py

--- dev_src/pyroDB.py
+++ dev_src/pyroDB.py
@@ -81,11 +81,11 @@ class PickleDB(object):
 		return self.get(item, raiseErr=True)
 
 	def __setitem__(self, key, value):
-		'''Sytax sugar for set()'''
+		'''Syntax sugar for set()'''
 		return self.set(key, value)
 
 	def __delitem__(self, key):
-		'''Sytax sugar for rem()'''
+		'''Syntax sugar for rem()'''
 		return self.rem(key)
 
 
@@ -128,6 +128,7 @@ class PickleDB(object):
 
 	def _dump(self):
 		'''Dump to a temporary file, and then move to the actual location'''
+		print ("dumping ")
 		with NamedTemporaryFile(mode='wb', delete=False) as f:
 			msgpack.dump(self.db, f)
 		if os.stat(f.name).st_size != 0:
@@ -369,10 +370,23 @@ class PickleDB(object):
 		return True
 		
 
+# DUMMY CLASS FOR TYPING
+class PickleTable:
+	pass
 
+class _PickleTCell:
+	pass
 		
+class _PickleTRow:
+	pass
+	
+class _PickleTColumn:
+	pass
+	
 
-class PickleTable(object):
+
+
+class PickleTable:
 	def __init__(self, filename="", *args, **kwargs):
 		self.CC = 0 # consider it as country code and every items are its people. they gets NID
 		
@@ -418,9 +432,11 @@ class PickleTable(object):
 	
 			self.busy = True
 			
-			func(*args, **kwargs)
+			box = func(*args, **kwargs)
 			
 			self.busy = False
+			
+			return box
 			
 		return inner
 		
@@ -509,10 +525,15 @@ class PickleTable(object):
 		for r in range(self.height):
 			yield self.row(r, _columns=columns)
 			
-	def rows_obj(self):
+	def rows_obj(self, start:int=0, end:int=None, sep:int=1):
 		'''Return a list of all rows in db'''
+		if end is None:
+			end = self.height
+		if end<0:
+			end = self.height + end
+
 		columns = self.column_names
-		for r in range(self.height):
+		for r in range(start, end, sep):
 			yield self.row_obj(r, _columns=columns)
 			
 			
@@ -594,7 +615,7 @@ class PickleTable(object):
 	def get_cell_by_id(self, col, row_id):
 		return self.get_cell(col, self.ids.index(row_id))
 		
-	def get_cell_obj(self, col, row=-1, row_id=-1):
+	def get_cell_obj(self, col, row:int=-1, row_id:int=-1):
 		"""
 		return cell object 
 		# col: column name
@@ -603,41 +624,52 @@ class PickleTable(object):
 		"""
 		
 		if row>-1:
-			return _PickleTCell(self, column=col, uid=self.ids[row], CC=self.CC)
-		elif row_id>-1:
-			return _PickleTCell(self, column=col, uid=self.ids[row], CC=self.CC)
-		else:
-			raise IndexError("Invalid row")
-
-
-	def pop_row(self, index=-1, AD=True):
-		self.ids.pop(index)
+			return _PickleTCell(self, column=col, row_id=self.ids[row], CC=self.CC)
+		if row_id>-1:
+			return _PickleTCell(self, column=col, row_id=self.ids[row], CC=self.CC)
 		
+		# in case row or row_id is invalid
+		raise IndexError("Invalid row")
+
+
+	def pop_row(self, index:int=-1, returns=True, AD=True):
+		"""
+		index: index of the row (not id), if not given, last row of the table is popped
+		returns: whether return the popped row. (how pop should work)
+		"""
+		box = None
+		if returns:
+			box = self.row(index)
+
 		for c in self.column_names:
 			self._pk.db[c].pop(index)
+		
+		self.ids.pop(index)
 			
 		self.height -=1
 		
 		if AD:
 			self.auto_dump()
+			
+		return box
 	
-	def del_row(self, index, AD=True):
+	def del_row(self, row:int, AD=True):
 		# Auto dumps (locked)
-		self.lock(self.pop_row)(index, AD=AD)
+		self.lock(self.pop_row)(row, returns=False, AD=AD)
 		
 		
 		
-	def del_row_id(self, uid, AD=True):
+	def del_row_id(self, row_id:int, AD=True):
 		"""
-		id: unique id of the row
+		row_id: unique id of the row
 		AD: auto dump
 		"""
-		self.del_row(self.ids.index(uid), AD=AD)
+		self.del_row(self.ids.index(row_id), AD=AD)
 
 	
-	def _add_row(self, row:dict):
+	def _add_row(self, row:Union[dict, _PickleTRow], ignore_extra=False):
 		"""
-		# row: row must be a dict containing column names and values
+		# row: row must be a dict or _PickleTRow containing column names and values
 		"""
 		if not self.ids:
 			self.ids.append(0)
@@ -645,26 +677,22 @@ class PickleTable(object):
 			self.ids.append(self.ids[-1] + 1)
 			
 		for k in self.column_names:
-			self._pk.db[k].append(None)
-			
-		
-			
-		
-		
-		for k, v in row.items():
-			self.set_cell(k, self.height, v, AD=False)
+			self._pk.db[k].append(row.get(k))
 
+		
+		#for k, v in row.items():
+		#	self.set_cell(k, self.height, v, AD=False)
 			
 		self.height += 1
 
 		
-	def add_row(self, row:dict, AD=True):
+	def add_row(self, row:Union[dict, _PickleTRow], ignore_extra=False, AD=True):
 		""" 
 		@ locked
 		# row: row must be a dict containing column names and values
 		"""		
 			
-		self.lock(self._add_row)(row)
+		self.lock(self._add_row)(row=row, ignore_extra=ignore_extra)
 		
 		if AD:
 			self.auto_dump()
@@ -685,10 +713,12 @@ class PickleTable(object):
 	def auto_dump(self):
 		self._pk._autodumpdb()
 		
-class _PickleTCell(object):
-	def __init__(self, source:PickleTable, column, uid, CC):
+
+		
+class _PickleTCell:
+	def __init__(self, source:PickleTable, column, row_id:int, CC):
 		self.source = source
-		self.id = uid
+		self.id = row_id
 		self.column = column
 		self.CC = CC
 		
@@ -735,10 +765,21 @@ class _PickleTRow:
 		self.CC = CC
 		
 	def __getitem__(self, name):
-		# Auto dumps
 		self.source.raise_source(self.CC)
 		
 		self.source.get_cell(name, self.source.ids.index(self.id))
+		
+	def get(self, name, default=None):
+		if name not in self.source.column_names:
+			return default
+			
+		return self[name]
+		
+	def get_cell_obj(self, name, default=None):
+		if name not in self.source.column_names:
+			return default
+			
+		return self.source.get_cell_obj(col=name, row_id=self.id)
 
 	def __setitem__(self, name, value):
 		# Auto dumps
@@ -752,19 +793,20 @@ class _PickleTRow:
 		
 		self.source.set_cell(name, self.source.ids.index(self.id), None)
 		
-	def update(self, new:Union[dict, "_PickleTRow"], ignore_extra=False):
+	def update(self, new:Union[dict, "_PickleTRow"], ignore_extra=False, AD=True):
 		"""
 		Auto dumps 
 		"""
 		
 		for k, v in new.items():
 			try:
-				self.source._set_cell(k, self.source.ids.index(self.id), v)
+				self.source.set_cell(k, self.source.ids.index(self.id), v, AD=False)
 			except KeyError:
 				if not ignore_extra:
 					raise
 					
-		self.source.auto_dump()
+		if AD:
+			self.source.auto_dump()
 		
 	def __str__(self):
 		return str({k:v for k, v in self.items()})
@@ -782,6 +824,77 @@ class _PickleTRow:
 
 		self.source.del_row_id(self.id)
 
+
+class _PickleTColumn:
+	def __init__(self, source:PickleTable, name, CC):
+		self.source = source
+		self.name = name
+		self.CC = CC
+		
+	def __getitem__(self, row:int):
+		"""
+		row: the index of row (not id)
+		"""
+		# self.source.raise_source(self.CC)
+		
+		self.source.get_cell(col=self.name, row=row)
+		
+	def get(self, row:int, default=None):
+		if row > (self.source.height-1):
+			return default
+			
+		return self[row]
+
+	def __setitem__(self, row:int, value):
+		"""
+		@ Auto dumps
+		# row: row index (not id)
+		# value: accepts both raw value and _PickleTCell obj
+		"""
+		
+		# self.source.raise_source(self.CC)
+		
+		if isinstance(value, _PickleTCell):
+			value = value.value
+		
+		self.source.set_cell(col=self.name, row=row, val=value)
+
+	def __delitem__(self, row:int):
+		"""
+		@ Auto dump
+		# row: index of row (not id)
+		"""
+		# self.source.raise_source(self.CC)
+		
+		self.source.set_cell(self.name, row, None)
+		
+	
+	def append(self, *args, **kwargs):
+		raise NotImplementedError("You can't manually just add a cell in the column. You must add a new row for that")
+		
+	extend = append
+	
+	
+	
+	def __str__(self):
+		return str({k:v for k, v in self.items()})
+		
+	def keys(self):
+		return self.source.column_names
+		
+	def items(self):
+		for k in self.keys():
+			yield (k, self[k])
+		
+	def del_column(self):
+		"""
+		# Auto dumps
+		** This will also invalidate this object. Handle with care **
+		"""
+		self.source.raise_source(self.CC)
+
+		self.source.del_column(self.name)
+
 	
 		
 		
@@ -789,10 +902,10 @@ class _PickleTRow:
 		
 
 import string
-def Lower_string(length): # define the function and pass the length as argument  
+def Lower_string(length): # define the function and pass the length as argument 
     # Print the string in Lowercase  
-    result = ''.join((random.choice(string.ascii_lowercase) for x in range(length))) # run loop until the define length 
-    return result
+	result = ''.join((random.choice(string.ascii_lowercase) for x in range(length))) # run loop until the define length 
+	return result
 
 
 if __name__ != "__main__":
@@ -813,7 +926,7 @@ if __name__ == "__main__":
 
 	
 	st = time.time()
-	tb = PickleTable()
+	tb = PickleTable("test.pdb")
 	print(f"load time: {time.time()-st}s")
 	
 	print(tb.height)
@@ -824,7 +937,7 @@ if __name__ == "__main__":
 	
 	print("adding")
 	for n in range(int(5555)):
-		tb._add_row({"x":n, "Y":00})
+		tb._add_row({"x":n, "Y":'🍎'})
 		
 		#print(n)
 	
@@ -839,22 +952,26 @@ if __name__ == "__main__":
 	print("Total cells", tb.height * len(tb.column_names))
 	
 	et = time.time()
-	print(f"Load and dump test in {et - st}s")
+	print(f"Load and dump test in {et - st}s\n")
 	
-
+	print("="*50)
 	
-	print("\n Assign test")
+	print("\n Assign random string in first 1,000 rows test")
+	print("="*50)
 	st = time.time()
 	
-	for row in tb.rows_obj():
-		row["m"] = Lower_string(10)
+	for row_ in tb.rows_obj(0, 1000):
+		row_.update({"m": Lower_string(10)}, AD=False)
 		
 	et = time.time()
 		
 	print(f"Assigned test in {et - st}s")
 	print(tb)
+	dt = time.time ()
+	tb.dump()
+	print(f"dump time: {time.time()-dt}s") 
 	
-	
+	print("="*50)
 	
 	print("\n\n Search test")
 	st = time.time()
@@ -864,7 +981,7 @@ if __name__ == "__main__":
 		
 	et = time.time()
 		
-	print(f"Search test in {et - st}s")
+	print(f"Search 'abc' test in {et - st}s")
 	
 	
 	
