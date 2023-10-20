41409c28ef18c23812615f224928fbf36e6a9ed8
RaSan147 <34002411+RaSan147@users.noreply.github.com>
2023-10-20 06:31:19 +0600

    Added page handler to maintain all types from one HTML

---
+++ dev_src/script_page_handler.js
@@ -0,0 +1,100 @@
+
+class Page{
+	constructor(){
+		this.container = byId('content_container')
+		this.type = null;
+		this.handler = null;
+		
+		this.dir_tree = document.getElementById("dir-tree")
+		
+		this.dir_tree.scrollLeft = this.dir_tree.scrollWidth;
+		// convert scroll to horizontal
+		this.dir_tree.onwheel = function(event) {
+			event.preventDefault();
+			// scroll to left
+			event.target.scrollBy({
+				top: 0,
+				left: event.deltaY,
+				behavior: 'smooth'
+			})
+			event.deltaY < 0;
+		}
+		
+		this.initialize()
+	}
+	
+	get_type(){
+		const url = tools.add_query_here('type', '');
+		return fetch(url)
+					.then(data => {return data.text()}) 
+	}
+
+	hide_all(){
+		fm_page.hide()
+		video_page.hide()
+		admin_page.hide()
+	}
+
+	clear(){
+		this.handler.clear()
+	}
+	
+	async initialize(){
+		this.container.style.display = "none"
+		this.hide_all()
+		this.update_displaypath()
+
+		this.type = await this.get_type()
+		var type = this.type
+		
+
+		if (type == 'dir') {
+			this.handler = fm_page;
+		} else if (type == 'vid') {
+			this.handler = video_page;
+		}
+
+		this.handler.initialize()
+		this.handler.show()
+
+
+		this.container.style.display = "block"
+
+	}
+
+	on_action_button() {
+		this.handler.on_action_button()
+	}
+
+	set_title(title) {
+		window.document.title = title;
+	}
+
+	set_diplaypath(displaypath) {
+		this.dir_tree.innerHTML = displaypath;
+	}
+
+	update_displaypath() {
+		const path = window.location.pathname;
+		const dirs = path.replace(/\/{2,}/g, "/").split('/');
+		const urls = ['/'];
+		const names = ['&#127968; HOME'];
+		const r = [];
+
+		for (let i = 1; i < dirs.length - 1; i++) {
+			const dir = dirs[i];
+			urls.push(urls[i - 1] + encodeURIComponent(dir).replace(/'/g, "%27").replace(/"/g, "%22") + (dir.endsWith('/') ? '' : '/'));
+			names.push(dir);
+		}
+
+		for (let i = 0; i < names.length; i++) {
+			const tag = "<a class='dir_turns' href='" + urls[i] + "'>" + names[i] + "</a>";
+			r.push(tag);
+		}
+
+		this.set_diplaypath(r.join('<span class="dir_arrow">&#10151;</span>'));
+	}
+
+}
+
+const page = new Page()
